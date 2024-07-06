98c086172cb90b9385f7086b176d65372f1eac38
achramdani <achramdani18@gmail.com>
2024-07-07 02:14:52 +0700

    UAS

--- music_app/urls.py
+++ music_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from music_app.views import ArtistList,AlbumList,LaguList,PlayList
+from music_app.views import ArtistList, ArtistDetail, AlbumList,AlbumDetail, LaguList,LaguDetail, PlayList, PlaylistDetail
 
 urlpatterns = [
     path('artis/', ArtistList.as_view()),
+    path('artis/<int:pk>/', ArtistDetail.as_view()),
     path('album/', AlbumList.as_view()),
+    path('album/<int:pk>/', AlbumDetail.as_view()),
     path('lagu/', LaguList.as_view()),
-    path('playlist/', PlayList.as_view()), 
+    path('lagu/<int:pk>/', LaguDetail.as_view()),
+    path('playlist/', PlayList.as_view()),
+    path('playlist/<int:pk>/', PlaylistDetail.as_view()), 
 ]
--- music_app/views.py
+++ music_app/views.py
@@ -1,20 +1,124 @@
 from music_app.models import Artis,Album,Lagu,Playlist
 from music_app.serializers import ArtisSerilizer,AlbumSerilizer,LaguSerilizer,PlaylistSerilizer
 from rest_framework import generics
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ArtistList(generics.ListCreateAPIView):
     queryset = Artis.objects.all()
     serializer_class = ArtisSerilizer
 
+class ArtistDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Artis.objects.get(pk=pk)
+        except Artis.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        Artis = self.get_object(pk)
+        serializer = ArtisSerilizer(Artis)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        Artis = self.get_object(pk)
+        serializer = ArtisSerilizer(Artis, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        Artis = self.get_object(pk)
+        Artis.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class AlbumList(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerilizer
+
+class AlbumDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Album.objects.get(pk=pk)
+        except Album.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        Album = self.get_object(pk)
+        serializer = AlbumSerilizer(Album)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        Album = self.get_object(pk)
+        serializer = AlbumSerilizer(Album, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        Album = self.get_object(pk)
+        Album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class LaguList(generics.ListCreateAPIView):
     queryset = Lagu.objects.all()
     serializer_class = LaguSerilizer
 
+class LaguDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Lagu.objects.get(pk=pk)
+        except Lagu.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        Lagu = self.get_object(pk)
+        serializer = LaguSerilizer(Lagu)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        Lagu = self.get_object(pk)
+        serializer = LaguSerilizer(Lagu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        Lagu = self.get_object(pk)
+        Lagu.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)   
+
 class PlayList(generics.ListCreateAPIView):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerilizer
+
+class PlaylistDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Playlist.objects.get(pk=pk)
+        except Playlist.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        PlayList = self.get_object(pk)
+        serializer = PlaylistSerilizer(PlayList)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        PlayList = self.get_object(pk)
+        serializer = PlaylistSerilizer(Playlist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        Playlist = self.get_object(pk)
+        Playlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
